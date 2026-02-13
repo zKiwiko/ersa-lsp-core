d2ba7caa4a15a18097e7ba1f1764814a024227ec
Aria | zKiwiko <slimebomb33@gmail.com>
2026-02-13 16:11:33 -0500

    push workflow setup

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,7 +5,9 @@ async fn main() {
     let args = std::env::args().collect::<Vec<_>>();
     if args.len() > 1 && args[1] == "--stdio" {
         lsp::LSP::start().await;
+    } else if args.len() > 1 && args[1] == "--version" {
+        println!("{}", env!("CARGO_PKG_VERSION"));
     } else {
-        eprintln!("Usage: ersa_lsp --stdio");
+        eprintln!("Usage: ersa_lsp [--stdio | --version]");
     }
 }
